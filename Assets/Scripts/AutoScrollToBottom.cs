a944720d8286b37bca6852865f9b80146e2b8437
Henry Chan <hchan@apache.org>
2025-04-12 10:07:42 -0700

    AutoScrollToBottom

--- Assets/Scripts/AutoScrollToBottom.cs
+++ Assets/Scripts/AutoScrollToBottom.cs
@@ -1,13 +1,31 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class AutoScrollToBottom : MonoBehaviour
 {
     public ScrollRect scrollRect;
 
+    public void ScrollNow()
+    {
+        StartCoroutine(ScrollAfterLayout());
+    }
+
+    IEnumerator ScrollAfterLayout()
+    {
+        // Wait a couple of frames for WebGL layout delay
+        yield return null;
+        yield return null;
+
+        // Force layout rebuild just to be safe
+        LayoutRebuilder.ForceRebuildLayoutImmediate(scrollRect.content);
+
+        // Set to bottom
+        scrollRect.verticalNormalizedPosition = 0f;
+    }
+
     void LateUpdate()
     {
-        // Scroll all the way to the bottom
         scrollRect.verticalNormalizedPosition = 0f;
     }
 }

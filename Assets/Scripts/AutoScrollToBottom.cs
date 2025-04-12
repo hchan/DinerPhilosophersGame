91095135f3acf143c50809c452d8447d9cc1ada1
Henry Chan <hchan@apache.org>
2025-04-12 10:09:25 -0700

    just lateUpdate

--- Assets/Scripts/AutoScrollToBottom.cs
+++ Assets/Scripts/AutoScrollToBottom.cs
@@ -6,24 +6,6 @@ public class AutoScrollToBottom : MonoBehaviour
 {
     public ScrollRect scrollRect;
 
-    public void ScrollNow()
-    {
-        StartCoroutine(ScrollAfterLayout());
-    }
-
-    IEnumerator ScrollAfterLayout()
-    {
-        // Wait a couple of frames for WebGL layout delay
-        yield return null;
-        yield return null;
-
-        // Force layout rebuild just to be safe
-        LayoutRebuilder.ForceRebuildLayoutImmediate(scrollRect.content);
-
-        // Set to bottom
-        scrollRect.verticalNormalizedPosition = 0f;
-    }
-
     void LateUpdate()
     {
         scrollRect.verticalNormalizedPosition = 0f;

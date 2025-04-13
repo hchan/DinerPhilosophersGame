edd698c45b297df8dd17b5c04fe14e0b55306307
Henry Chan <hchan@apache.org>
2025-04-12 17:12:26 -0700

    timer done

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -12,10 +12,17 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (GetImage().fillAmount > 0f)
+        {
+            GetImage().fillAmount -= Time.deltaTime / RunDinerPhilosophersGame.SIMULATION_TIME;
+        }
     }
 
     public void UpdateTimer() {
-        GameManager.Instance.Get("timerImage").GetComponent<Image>().fillAmount = 1f;
+        GetImage().fillAmount = 1f;
+    }
+
+    public Image GetImage() {
+        return GameManager.Instance.Get("timerImage").GetComponent<Image>();
     }
 }

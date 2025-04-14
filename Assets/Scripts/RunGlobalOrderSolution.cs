38036af2c008055a4cf11373212484cecd890a81
Henry Chan <hchan@apache.org>
2025-04-14 12:02:41 -0700

    run global order soln

---
+++ Assets/Scripts/RunGlobalOrderSolution.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RunGlobalOrderSolution : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Button runGlobalOrderSolution;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("Run Global Order Solution Button clicked");
+    }
+}

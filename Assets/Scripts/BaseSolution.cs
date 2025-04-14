66a14759bb5228e9e8c1f912284e4641d60d5c93
Henry Chan <hchan@apache.org>
2025-04-14 16:04:34 -0700

    solutionStrategy

--- Assets/Scripts/BaseSolution.cs
+++ Assets/Scripts/BaseSolution.cs
@@ -18,23 +18,15 @@ public class BaseSolution : MonoBehaviour
 
     public void GlobalOrderSolution()
     {
-        GameManager.Instance.Get("TabManager").GetComponent<TabManager>().ShowTab("diningTable");
-        RunDinerPhilosophersGame runDinerPhilosophersGame = GameManager.Instance.Get("runDinerPhilosophersGame").GetComponent<RunDinerPhilosophersGame>();
-        for (int i = 0; i < RunDinerPhilosophersGame.NUM_PHILOSOPHERS; i++)
-        {
-            GameManager.Instance.chopstickData[i].availablePickupChopsticks.Clear();
-            GameManager.Instance.chopstickData[i].orderPickupChopsticks.Clear();
-            GameManager.Instance.chopstickData[i].availableDropChopsticks.Clear();
-            GameManager.Instance.chopstickData[i].orderDropChopsticks.Clear();
-
-            List<int>[] pickupAndDropLists = runDinerPhilosophersGame.GlobalOrderSolution(i);
-            GameManager.Instance.chopstickData[i].orderPickupChopsticks.AddRange(pickupAndDropLists[0]);
-            GameManager.Instance.chopstickData[i].orderDropChopsticks.AddRange(pickupAndDropLists[1]);            
-        }
-         GameManager.Instance.Get("runButton").GetComponent<Button>().onClick.Invoke(); // Invoke the click event of the run button ... this includes ALL listeners
+        ExecuteSolution((runDinerPhilosophersGame, i) => runDinerPhilosophersGame.GlobalOrderSolution(i));
     }
 
     public void PartialOrderSolution()
+    {
+        ExecuteSolution((runDinerPhilosophersGame, i) => runDinerPhilosophersGame.PartialOrderSolution(i));
+    }
+
+    private void ExecuteSolution(System.Func<RunDinerPhilosophersGame, int, List<int>[]> solutionStrategy)
     {
         GameManager.Instance.Get("TabManager").GetComponent<TabManager>().ShowTab("diningTable");
         RunDinerPhilosophersGame runDinerPhilosophersGame = GameManager.Instance.Get("runDinerPhilosophersGame").GetComponent<RunDinerPhilosophersGame>();
@@ -45,10 +37,10 @@ public class BaseSolution : MonoBehaviour
             GameManager.Instance.chopstickData[i].availableDropChopsticks.Clear();
             GameManager.Instance.chopstickData[i].orderDropChopsticks.Clear();
 
-            List<int>[] pickupAndDropLists = runDinerPhilosophersGame.PartialOrderSolution(i);
+            List<int>[] pickupAndDropLists = solutionStrategy(runDinerPhilosophersGame, i);
             GameManager.Instance.chopstickData[i].orderPickupChopsticks.AddRange(pickupAndDropLists[0]);
             GameManager.Instance.chopstickData[i].orderDropChopsticks.AddRange(pickupAndDropLists[1]);            
         }
-         GameManager.Instance.Get("runButton").GetComponent<Button>().onClick.Invoke(); // Invoke the click event of the run button ... this includes ALL listeners
+        GameManager.Instance.Get("runButton").GetComponent<Button>().onClick.Invoke(); // Invoke the click event of the run button ... this includes ALL listeners
     }
 }
